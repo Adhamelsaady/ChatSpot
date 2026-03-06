961f0aeb4dc005e12d23904a7e1fe21de3113443
adhamelsaady <adhamelsaady81@gmail.com>
2026-03-07 01:56:09 +0200

    fix : fixed swashbuckle package conflict

--- ChatSpot/ChatSpot/Program.cs
+++ ChatSpot/ChatSpot/Program.cs
@@ -11,17 +11,24 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ChatSpotDbContext>(opts =>
     opts.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+builder.Services.ConfigureServices();
 builder.Services.ConfigurePersistence();
 builder.Services.ConfigureAuthentication(builder.Configuration);
+
+
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
