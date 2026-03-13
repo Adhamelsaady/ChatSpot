ddb51af608c6f3b1db046dcd52b47d0a7d686d15
adhamelsaady <adhamelsaady81@gmail.com>
2026-03-13 16:58:02 +0200

    feat : added the searching endpoint

--- ChatSpot/ChatSpot/Controllers/UserController.cs
+++ ChatSpot/ChatSpot/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using ChatSpot.Contracts.Services;
+using ChatSpot.ResourceParameters;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatSpot.Controllers;
@@ -8,5 +11,17 @@ namespace ChatSpot.Controllers;
 [Authorize]
 public class UserController : ControllerBase
 {
-    
+    private readonly IUserService  _userService;
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> Search([FromQuery] BaseResourceParameter resourceParameter)
+    {
+        var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value!;
+        var result = await _userService.SearchUsers(resourceParameter , currentUserId);
+        return Ok(result);
+    }
 }
