94bc053493970ff771316bb95bfb29e678379736
adhamelsaady <adhamelsaady81@gmail.com>
2026-03-26 18:14:42 +0200

    fix(aspnet) : added first & last name to the registerDto

--- ChatSpot/ChatSpot/Dtos/Ingoing/RegisterDto.cs
+++ ChatSpot/ChatSpot/Dtos/Ingoing/RegisterDto.cs
@@ -15,8 +15,8 @@ public class RegisterDto
     [Required(ErrorMessage = "Full name is required")]
     [MinLength(2, ErrorMessage = "Full name must be at least 3 characters")]
     public string UserName { get; set; } = string.Empty;
-    
-    public string Name { get; set; } = string.Empty;
-    
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public string Bio { get; set; } = string.Empty;
 }
