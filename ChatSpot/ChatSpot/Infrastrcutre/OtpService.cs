9e412096745bee76c719cd4882b704b72e45fc16
adhamelsaady <adhamelsaady81@gmail.com>
2026-03-31 07:20:39 +0200

    fix(aspnet) : changed the charcters for the OTP service

--- ChatSpot/ChatSpot/Infrastrcutre/OtpService.cs
+++ ChatSpot/ChatSpot/Infrastrcutre/OtpService.cs
@@ -7,7 +7,7 @@ public class OtpService : IOtpService
     public string GenerateOtp()
     {
         var random = new Random();
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!$#_";
+        const string chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789";
         return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
     }
 }
